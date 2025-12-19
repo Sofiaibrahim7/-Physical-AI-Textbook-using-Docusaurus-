b79fad1877796ee9b28afd72cbd6fb3ea848788e
Sofiaibrahim7 <hafizammar085@gmail.com>
2025-12-19 12:08:11 +0500

    Add sidebars for chapters

---
+++ sidebars.js
@@ -0,0 +1,40 @@
+module.exports = {  
+  docs: [  
+    {  
+      type: 'category',  
+      label: 'Introduction',  
+      items: ['intro'],  
+    },  
+    {  
+      type: 'category',  
+      label: 'Core Concepts',  
+      items: [  
+        'chapter1',  
+        'chapter2',  
+        'chapter3',  
+        'chapter4',  
+        'chapter5',  
+      ],  
+    },  
+    {  
+      type: 'category',  
+      label: 'Advanced Topics',  
+      items: [  
+        'chapter6',  
+        'chapter7',  
+        'chapter8',  
+        'chapter9',  
+        'chapter10',  
+        'chapter11',  
+        'chapter12',  
+        'chapter13',  
+        'chapter14',  
+      ],  
+    },  
+    {  
+      type: 'category',  
+      label: 'Simulations & Tools',  
+      items: ['simulations'],  
+    },  
+  ],  
+};  
